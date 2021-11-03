7ce74a2e8ac8e6ef3fd59e21bc06bb556a925e35
TusharManure <76638461+TusharManure@users.noreply.github.com>
2021-11-03 17:21:20 +0530

    Add files via upload

---
+++ PRO-V2-C43-Template-main/PRO-V2-C43-Template-main/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(25)
+    text("(R E V B A I L A)", 100,100)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,130)
+
+    fill("white")
+    textSize(25)
+    text("(C U T N I F O N)", 700,200)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,230)
+
+    fill("white")
+    textSize(25)
+    text("(A T E D A S B A)", 100,300)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,330)
+
+}
